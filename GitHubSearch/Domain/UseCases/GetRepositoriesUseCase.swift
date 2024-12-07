ce6d8a097c3e4122837d4d731803809b5a363c67
José Luis Sánchez-Porro Godoy <joseluis.sanchezporro.godoy@gmail.com>
2024-12-07 09:52:29 +0100

    🚧 Add UseCases . I used the same entities in the data layer and the domain layer to save time because I didn’t see it as necessary for this project since it is relatively small in scope. However, in the case of a larger project, I would use DTOs (Data Transfer Objects) for the network layer, mappers to convert between layers, and specific entities to represent data in the domain layer.

---
+++ GitHubSearch/Domain/UseCases/GetRepositoriesUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  GetRepositoriesUseCase.swift
+//  GitHubSearch
+//
+//  Created by JLSANCHEZP on 7/12/24.
+//
+import Foundation
+import GitHubNetworking
+
+/// A protocol defining the contract for fetching repositories of a GitHub user.
+protocol GetRepositoriesUseCaseProtocol {
+/// Fetches the repositories of a GitHub user.
+///
+/// - Parameter username: The username of the GitHub user.
+/// - Returns: An array of `Repository` objects.
+/// - Throws: An error if the request fails.
+func execute(request: String) async throws -> [Repository]
+}
+
+/// A concrete implementation of `GetRepositoriesUseCaseProtocol`.
+struct GetRepositoriesUseCase: GetRepositoriesUseCaseProtocol {
+    private let api: GitHubAPI
+
+    init(api: GitHubAPI) {
+        self.api = api
+    }
+
+    func execute(request: String) async throws -> [Repository] {
+        return try await api.fetchRepositories(username: request)
+    }
+}
---
+++ GitHubSearch/Domain/UseCases/GetUserUseCase.swift
@@ -0,0 +1,32 @@
+//
+//  GetUserUseCase.swift
+//  GitHubSearch
+//
+//  Created by JLSANCHEZP on 7/12/24.
+//
+
+import Foundation
+import GitHubNetworking
+
+/// A protocol defining the contract for fetching a GitHub user.
+protocol GetUserUseCaseProtocol {
+/// Fetches the details of a GitHub user.
+///
+/// - Parameter username: The username of the GitHub user to fetch.
+/// - Returns: A `User` object containing the user's information.
+/// - Throws: An error if the request fails.
+func execute(request: String) async throws -> User
+}
+
+/// A concrete implementation of `GetUserUseCaseProtocol`.
+struct GetUserUseCase: GetUserUseCaseProtocol {
+    private let api: GitHubAPI
+
+    init(api: GitHubAPI) {
+        self.api = api
+    }
+
+    func execute(request: String) async throws -> User {
+        return try await api.fetchUser(username: request)
+    }
+}
