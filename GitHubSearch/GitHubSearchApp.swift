f2e29d48a90b3cb8edf38a79c9caceb2ac7f97ce
José Luis Sánchez-Porro Godoy <joseluis.sanchezporro.godoy@gmail.com>
2024-12-07 08:59:51 +0100

    Initial Commit

---
+++ GitHubSearch/GitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp.swift
+//  GitHubSearch
+//
+//  Created by JLSANCHEZP on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubSearchTests/GitHubSearchTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubSearchTests.swift
+//  GitHubSearchTests
+//
+//  Created by JLSANCHEZP on 7/12/24.
+//
+
+import Testing
+
+struct GitHubSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
