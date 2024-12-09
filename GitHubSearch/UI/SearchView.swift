5f1cf684227101d99b245f0844d9270ef604cbe2
José Luis Sánchez-Porro Godoy <joseluis.sanchezporro.godoy@gmail.com>
2024-12-09 18:31:38 +0100

    🎨 Follow as closely as possible the reference layout provided

--- GitHubSearch/UI/SearchView.swift
+++ GitHubSearch/UI/SearchView.swift
@@ -11,24 +11,32 @@ struct SearchView: View {
     @State var viewModel = UserRepositoriesViewModel()
     @State private var username: String = ""
     @State private var showDetails: Bool = false
+#if os(iOS)
+    init() {
+        // Personalizar la apariencia de la barra de navegación
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = UIColor.systemGray6
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black,
+                                                .font: UIFont.systemFont(ofSize: 17, weight: .light)]
+        navBarAppearance.shadowColor = UIColor.black.withAlphaComponent(0.50)
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+#endif
     var body: some View {
         NavigationStack {
-            VStack(spacing: 20) {
-                // Logo Placeholder
-                Image(systemName: "magnifyingglass.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.blue)
+            VStack {
+                Spacer()
 #if os(iOS)
                 // TextField for Username Input
-                TextField("Enter GitHub username", text: $username)
+                TextField("Username", text: $username)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .textInputAutocapitalization(.never)
                     .autocapitalization(.none)
-                    .padding(.horizontal)
+                    .padding(.horizontal, 20)
 #elseif os(tvOS)
-                TextField("Enter GitHub username", text: $username)
+                TextField("Username", text: $username)
                     .textFieldStyle(DefaultTextFieldStyle())
                     .textInputAutocapitalization(.never)
                     .autocapitalization(.none)
@@ -44,18 +52,20 @@ struct SearchView: View {
                     }
                 } label: {
                     Text("Search")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(username.isEmpty ? Color.gray : Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
+                        .foregroundColor(.blue)
+                        .font(.system(size: 17))
+                        .fontWeight(.regular)
                 }
-                .padding(.horizontal)
+                .padding(.top, 10)
                 .disabled(username.isEmpty)
                 Spacer()
             }
             .waiting(loading: $viewModel.loading)
             .navigationTitle("GitHub Viewer")
+#if os(iOS)
+
+            .navigationBarTitleDisplayMode(.inline)
+#endif
             .navigationDestination(isPresented: $showDetails) {
                 DetailView(viewModel: viewModel)
             }
@@ -65,6 +75,7 @@ struct SearchView: View {
                 Text(viewModel.errorMsg)
             }
         }
+        .toolbarBackground(.orange, for: .navigationBar, .tabBar)
     }
 }
 
