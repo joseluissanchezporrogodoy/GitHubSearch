d44a69f67119850a8dfbe5a8c2c223134f09b008
José Luis Sánchez-Porro Godoy <joseluis.sanchezporro.godoy@gmail.com>
2024-12-09 21:36:06 +0100

    🎨 Follow as closely as possible the reference layout provided

--- GitHubSearch/UI/DetailView.swift
+++ GitHubSearch/UI/DetailView.swift
@@ -11,53 +11,63 @@ struct DetailView: View {
     @State var viewModel: UserRepositoriesViewModel
     @FocusState private var focusedItem: String? // Estado de foco para identificar el primer ítem
     var body: some View {
-        VStack {
-            if let user = viewModel.user {
-                // User Profile Section
-                VStack {
-                    AsyncImage(url: URL(string: user.avatarUrl ?? "")) { image in
-                        image.resizable()
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: 120, height: 120)
-                    .clipShape(Circle())
-                    .padding(.bottom, 8)
-                    Text(user.name ?? "Unknown User")
-                        .font(.title2)
-                        .bold()
-                    Text(user.login)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-                .padding()
-                Divider()
-            }
-            // Repositories List
-            List(viewModel.repositories, id: \.name) { repo in
-                VStack(alignment: .leading) {
-                    Text(repo.name)
-                        .font(.headline)
-                    Text(repo.language ?? "Unknown Language")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-                .padding()
-                .focusable(true)
-                .focused($focusedItem, equals: repo.name)
-            }
-            .listStyle(PlainListStyle())
-        }
-        .navigationTitle("User Details")
+        ZStack {
 #if os(iOS)
-        .navigationBarTitleDisplayMode(.inline)
+            Color(UIColor.systemGray6)
+                .ignoresSafeArea()
+#endif
+            VStack {
+                if let user = viewModel.user {
+                    // User Profile Section
+                    VStack {
+                        AsyncImage(url: URL(string: user.avatarUrl ?? "")) { image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 120, height: 120)
+                        .clipShape(Circle())
+                        .padding(.bottom, 8)
+                        Text(user.name ?? "Unknown User")
+                            .font(.title3)
+                            .bold()
+                    }
+                    .padding()
+                }
+                // Repositories List
+                List(viewModel.repositories, id: \.name) { repo in
+                    VStack(alignment: .leading) {
+                        Text(repo.name)
+                            .font(.headline)
+                        Text(repo.language ?? "Unknown Language")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+#if os(iOS)
+                    .listRowBackground(
+                               Color(UIColor.secondarySystemGroupedBackground)
+                                   .overlay(alignment: .bottom) {
+                                       Divider()
+                                   }
+                           )
+#endif
+                    .focusable(true)
+                    .focused($focusedItem, equals: repo.name)
+                }
+                .listStyle(PlainListStyle())
+            }
+
+#if os(iOS)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.clear, for: .navigationBar) // Barra transparente
 #elseif os(tvOS)
-        .navigationBarBackButtonHidden()
+            .navigationBarBackButtonHidden()
 #endif
-        .onAppear {
+            .onAppear {
 #if os(tvOS)
-            focusedItem = viewModel.repositories.first?.name
+                focusedItem = viewModel.repositories.first?.name
 #endif
+            }
         }
     }
 }
