80b6312b9cecbf34da3bea7d32c4e3cd8565740f
José Luis Sánchez-Porro Godoy <joseluis.sanchezporro.godoy@gmail.com>
2024-12-16 16:52:10 +0100

    🐛 Fix main actor uses in ViewModel

--- GitHubSearch/UI/UserRepositoriesViewModel.swift
+++ GitHubSearch/UI/UserRepositoriesViewModel.swift
@@ -31,25 +31,35 @@ final class UserRepositoriesViewModel: ObservableObject {
     // MARK: - Methods
     func fetchUserAndRepositories(for username: String) async {
         // Reset error state before starting a new search
-        errorMsg = ""
-        showAlert = false
-        self.user = nil
-        self.repositories = []
+        await MainActor.run {
+            errorMsg = ""
+            showAlert = false
+            self.user = nil
+            self.repositories = []
+        }
         do {
             await toggleLoading()
             let fetchedUser = try await getUserUseCase.execute(request: username)
-            self.user = fetchedUser
             let fetchedRepositories = try await getRepositoriesUseCase.execute(request: username)
             await toggleLoading()
-            self.repositories = fetchedRepositories
+            // Actualizaciones en el hilo principal
+            await MainActor.run {
+                self.user = fetchedUser
+                self.repositories = fetchedRepositories
+            }
+
         } catch let error as GitHubAPIError {
             await toggleLoading()
-            errorMsg = error.errorDescription
-            showAlert = true
+            await MainActor.run {
+                errorMsg = error.errorDescription
+                showAlert = true
+            }
         } catch {
             await toggleLoading()
-            errorMsg = error.localizedDescription
-            showAlert = true
+            await MainActor.run {
+                errorMsg = error.localizedDescription
+                showAlert = true
+            }
         }
     }
     @MainActor func toggleLoading() {
